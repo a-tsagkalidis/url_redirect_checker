e9061e7a1ccdcba31c6f04e0e921a4c1047fe170
Argyrios Tsagkalidis <arg.tsag@gmail.com>
2024-04-23 23:15:22 +0300

    Initial commit

---
+++ url_redirect_checker.py
@@ -0,0 +1,88 @@
+import requests
+import openpyxl
+from tqdm import tqdm
+
+
+def check_url_redirect(
+        original_url,
+        expected_redirect_url
+    ):
+    try:
+        response = requests.head(
+            original_url,
+            allow_redirects=True
+        )
+        if (
+                response.status_code == 200
+            ) and (
+                response.url == expected_redirect_url
+        ):
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+        return False
+
+
+def get_urls_from_excel(
+        file_path,
+        sheet_name,
+        column_index
+    ):
+    urls = []
+    try:
+        workbook = openpyxl.load_workbook(file_path)
+        sheet = workbook[sheet_name]
+        
+        for row in sheet.iter_rows(
+            min_row=2,
+            min_col=column_index,
+            values_only=True
+        ):
+            url = row[0]
+            if url:
+                urls.append(url)
+        
+        workbook.close()
+    except Exception as err:
+        print(f"Error: {err}")
+    
+    return urls
+
+
+def main(hdl_urls, redirect_urls):
+    for i in tqdm(range(len(hdl_urls))):
+        if check_url_redirect(
+            hdl_urls[i],
+            redirect_urls[i]
+        ):
+            pass
+        else:
+            print('\n\n    >>>>',
+                ' '.join(
+                    f'''
+                    {i}. REDIRECTION ERROR: {hdl_urls[i]} does not
+                    redirect correctly.
+                    '''.split()
+                ),
+                '\n'
+            )
+
+
+if __name__ == "__main__":
+    hdl_urls = get_urls_from_excel(
+        "all_hdl_urls.xlsx",
+        "Final",
+        1
+    )
+    redirect_urls = get_urls_from_excel(
+        "all_hdl_urls.xlsx",
+        "Final",
+        5
+    )
+
+    main(
+        hdl_urls,
+        redirect_urls
+    )
